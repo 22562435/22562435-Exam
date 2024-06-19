9a04c351f584c2db1c13a1307c2303c845eef799
Liam Andrew Beattie <159044283+22562435@users.noreply.github.com>
2024-06-20 01:01:18 +0200

    1 am

--- Question 1/code/plot_ridge_names.R
+++ Question 1/code/plot_ridge_names.R
@@ -4,7 +4,7 @@ plot_ridge_names<-function(df){
     g<- df %>%  ggplot( aes(x = Year_current, y = Name, height = Total_Count_current, group = Name, fill = Name)) +
         geom_density_ridges(stat = "identity", scale =5 , rel_min_height = 0,, alpha = 0.5) +
         scale_fill_hue(direction = 1) +
-        theme_minimal() +
+        theme_bw() +
         guides(fill = "none") +
         labs(
             title = "Popularity of Names Over Time",
