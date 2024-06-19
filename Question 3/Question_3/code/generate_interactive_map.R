07d7fe44e84cd8bc8d6cf53afe61f30cd9e8d88e
Liam Andrew Beattie <159044283+22562435@users.noreply.github.com>
2024-06-19 21:20:29 +0200

    again we commit

---
+++ Question 3/Question_3/code/generate_interactive_map.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(rnaturalearth)
+library(sf)
+library(leaflet)
+
+generate_interactive_map<-function(database,variable,title="",colour="YlOrRd")    {
+    pal <- colorNumeric(palette = colour, domain = database$variable, na.color = "transparent")
+
+    # Create the leaflet map
+    g<-leaflet(database) %>%
+        addTiles() %>%
+        addPolygons(
+            fillColor = ~pal(variable),
+            weight = 1,
+            opacity = 1,
+            color = "white",
+            dashArray = "3",
+            fillOpacity = 0.7,
+            highlightOptions = highlightOptions(
+                weight = 5,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE), #format(round(x, 2), nsmall = 2)
+            label = ~paste(name, ": ", format(round(variable, 2), nsmall = 2), " billion"),
+            labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto")
+        ) %>%
+        addLegend(pal = pal, values = ~variable, opacity = 1, title = title,
+                  position = "bottomright")
+
+    return(g)}
